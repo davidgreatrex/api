209a3f53840af8f39a261b0c9c798ebeb0a38616
David <dgreatrex0@gmail.com>
2017-06-26 22:59:36 +0100

    formatting updated

--- api.php
+++ api.php
@@ -36,43 +36,41 @@ Router::set('user/get', function() {
 /**
  * Add more routes here...
  */
-
 try {
-  /**
-   * force connections over https
-   */
-  $secure = filter_has_var(INPUT_SERVER, 'HTTPS');
-  
-  if (!$secure) {
-    Router::response("Please connect using an HTTPS connection", 403);
-    exit;
-  }
+	/**
+	 * force connections over https
+	 */
+	$secure = filter_has_var(INPUT_SERVER, 'HTTPS');
+
+	if (!$secure) {
+		Router::response("Please connect using an HTTPS connection", 403);
+		exit;
+	}
 
 	/**
 	 * Set the data that was sent in the request so it can be used later
 	 */
 	Router::data();
-	
+
 	/**
 	 * Find the handler for this request
 	 */
 	Router::get();
-	
+
 	/**
 	 * Check the request method is correct
 	 */
 	Router::validateMethod();
-	
+
 	/**
 	 * Check the data sent is as it should be
 	 */
 	Router::validateData();
-	
+
 	/**
 	 * Process the request and output the response
 	 */
 	Router::process();
-	
 } catch (Router_Exception $ex) {
 	Router::response($ex->getMessage(), 400);
 } catch (Exception $ex) {
--- classes/UserUpdate.inc.php
+++ classes/UserUpdate.inc.php
@@ -1,29 +1,30 @@
 <?php
 
 class UserUpdate extends API implements APIInterface {
-	
+
 	public function method() {
 		return "PUT";
 	}
-	
+
 	public function validate() {
 		return true;
 	}
-	
+
 	public function arguments() {
 		return array(
-			0 => array('filter'=>FILTER_SANITIZE_NUMBER_INT)
+			0 => array('filter' => FILTER_SANITIZE_NUMBER_INT)
 		);
 	}
-	
+
 	public function process($id) {
 		/**
 		 * The data submitted in the request
 		 */
 		$data = Router::$_data;
-		
+
 		/**
 		 * Add functionality to update a user by it's id
 		 */
 	}
+
 }
